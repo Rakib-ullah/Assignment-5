8902de745faabf930b21425bec635f02bf22e872
Rakib ullah <rakibullah19730@gmail.com>
2025-09-16 14:16:05 +0600

    banner part done

---
+++ main.js
@@ -0,0 +1,14 @@
+// Heart Btn eventlistener
+document.getElementById("cardHeart").addEventListener("click", () => {
+    alert("Heart clicked");
+});
+
+// Coin Btn eventlistener
+document.getElementById("cardCoin").addEventListener("click", () => {
+    alert("coin clicked");
+});
+
+// Copy Btn eventlistener
+document.getElementById("cardBtn").addEventListener("click", () => {
+    alert("Button clicked");
+});
